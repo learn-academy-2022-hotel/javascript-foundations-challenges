3dcb54529690b6c100718c796b2851e5077f46d6
romeoyutig <romeoyutig2@outlook.com>
2022-12-05 13:01:36 -0800

    updated challenges

--- func-ooops-arrays-romeo-dave.js
+++ func-ooops-arrays-romeo-dave.js
@@ -23,39 +23,63 @@ const testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
 // input/parameter: array of oddNumbers
 // expected output: new array showing only odd odd numbers
 
-const oddNumbers = (array) => {
-  let newArr  = []
-  for (let i = 0; i <array.length; i++) {
-    if (array[i] % 2 === 1) {
-    newArr.unshift(array[i])
-    }
-  }
-  return newArr
-}
-console.log(oddNumbers(testArr2))
+// const oddNumbers = (array) => {
+//   let newArr  = []
+//   for (let i = 0; i <array.length; i++) {
+//     if (array[i] % 2 === 1) {
+//     newArr.push(array[i])
+//     }
+//   }
+//   return newArr
+// }
+// console.log(oddNumbers(testArr2))
 
 // output: [-7, 3, 5, 13]
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: use the typeof method.
-const comboArr = [
-  7,
-  "n",
-  true,
-  "i",
-  "c",
-  10,
-  "e",
-  -388,
-  "w",
-  3,
-  "o",
-  0,
-  "r",
-  false,
-  "k"
-]
+
+// declare a function named letters
+// input/parameter: an array of numbers and letters
+// if the data type is a string, return the string
+// if the data type is anything else, return nothing
+// const comboArr = [
+//   7,
+//   "n",
+//   true,
+//   "i",
+//   "c",
+//   10,
+//   "e",
+//   -388,
+//   "w",
+//   3,
+//   "o",
+//   0,
+//   "r",
+//   false,
+//   "k"
+// ]
+
+// const letters = (array) => {
+//   let newArray = [] 
+//   for (let i=0 ; i<array.length ; i++){
+//     if (typeof array === "string"){
+//       return newArray}
+//  } 
+// }
+// console.log(letters.concatcomboArr)
+
 // output: "nicework"
 // Create a function that takes in an array of numbers and returns the sum.
+// 
 const addThese1 = [1, 2, 3, 4]
+function sumArray (array) {
+  let sum = 0
+  for (let i=0 ; i<array.length ; i++) {
+    sum += array[i]
+  }
+  console.log(sumArray(addThese1))
+  return sum
+}
 // output: 10
 
 const addThese2 = []
