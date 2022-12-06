ae269fe8602ee24b44964c4cc1bbb97205157d61
chrisyhlee <leechr8393@gmail.com>
2022-12-05 21:11:24 -0800

    updated challanges and stretch goals

--- higher-order-torin-chris.js
+++ higher-order-torin-chris.js
@@ -4,24 +4,53 @@
 // Don't forget to pseudo code.
 
 // Write a function that takes in an array of numbers and returns a new array with all numbers multiplied by 10.
-const arr1 = [3, 9, 15, 4, 10]
-
-
+// const arr1 = [3, 9, 15, 4, 10]
 // // output: [30, 90, 150, 40, 100]
+
+// const timesThree = arr1.map(value => value * 3)
+// console.log(timesThree)
+
 // Write a function that takes in an array of numbers and returns a new array with only odd numbers.
 // const arr2 = [2, 7, 3, 5, 8, 10, 13]
 // // output: [7, 3, 5, 13]
+
+// const onlyOdds = arr2.filter(value => value % 2 !== 0)
+// console.log(onlyOdds)
+
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use the typeof operator.
-// const comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+const comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
 // // output: "nicework"
+
+const niceWork = comboArr.filter(value => typeof value === 'string')
+console.log(niceWork.join(""))
+
 // Create a function that takes in an array and returns an array without any false, null, 0, or blank values.
-// const filterArrayValues = [58, "", "abcd", true, null, false, 0]
+const filterArrayValues = [58, "", "abcd", true, null, false, 0]
 // // output: [58, "abcd", true]
+
+const filteredArr = filterArrayValues.filter(value => value !== false && value !== null && value !==0 && value !== "")
+console.log(filteredArr);
+
 // 🏔 Stretch Goals
 // Create a function that takes in a string and returns a new string with all the vowels removed.
-// const str = "javascript is awesome"
+const str = "javascript is awesome"
 // // output: "jvscrpt s wsm"
+
+const noVowels = (str) => {
+    return str.replace(/[aeiou]/gi, '')
+}
+console.log(noVowels(str))
+
 // Create a function that takes in two arrays as arguments returns one array with no duplicate values.
-// const arr1 = [3, 7, 10, 5, 4, 3]
-// const arr2 = [7, 8, 2, 1, 5, 4]
+const arr1 = [3, 7, 10, 5, 4, 3]
+const arr2 = [7, 8, 2, 1, 5, 4]
 // // output: [3, 7, 10, 5, 4, 8, 2, 1]
+
+let noDupe = []
+let combinedArray = arr1.concat(arr2)
+    combinedArray.forEach((unique) => {
+        if (!noDupe.includes(unique)){
+            noDupe.push(unique)
+        }
+    })
+console.log(noDupe)
